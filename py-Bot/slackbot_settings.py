2675f2caf725b21492da9729d13e3a22552d2660
Yichun Sung <ycsung.r@gmail.com>
2019-09-28 02:02:56 +0800

    Add bot modules.

---
+++ py-Bot/slackbot_settings.py
@@ -0,0 +1,8 @@
+API_TOKEN = "YOUR_TOKEN"
+DEFAULT_REPLY = "Hello World!! I'm your Bot"
+
+ERRORS_TO = 'slackbot-test'
+
+PLUGINS = [
+    'mybot'
+]
---
+++ slackbot_settings.py
@@ -0,0 +1,8 @@
+API_TOKEN = "YOUR TOKEN"
+DEFAULT_REPLY = "Hello World!! I'm your Bot"
+
+ERRORS_TO = 'slackbot-test'
+
+PLUGINS = [
+    'mybot'
+]
